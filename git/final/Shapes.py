2fef653b66abbbaeab262df095891629967901e9
PhanDucManh0503 <pdmanh19@clc.fitus.edu.vn>
2026-05-04 14:05:16 +0700

    define-structure

--- git/final/Shapes.py
+++ git/final/Shapes.py
@@ -3,5 +3,23 @@ import math
 
 class Shapes(object):
     """
-        This class is used as utils to calculate properties of all shapes
+    This class is used as utils to calculate properties of all shapes
     """
+
+    # calculate the area of a rectangle
+
+    #   calculate the perimeter of a rectangle
+
+    #   calculate the width of a rectangle
+
+    #   calculate the length of a rectangle
+
+    #   calculate the area of square
+
+    #   calculate the perimeter of a square
+
+    #   calculate the side of a square
+
+    #   calculate the area of a circle
+
+    #   calculate the radius of a circle
