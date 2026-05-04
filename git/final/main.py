5993da5b36fc125011d1405240860df12bc258e8
Subin Khang <subinkhang102@gmail.com>
2026-05-04 14:20:39 +0700

    feat: implement rectangle length calculation

--- git/final/main.py
+++ git/final/main.py
@@ -1,6 +1,13 @@
 from Shapes import Shapes
 
 def main():
+    area = 50
+    width = 5
+    
+    # Task 4: Calculate the length of a rectangle
+    length = Shapes.calculate_rectangle_length(area, width)
+    print(f"Rectangle with Area={area} and Width={width}:")
+    print(f"- Calculated Length: {length}")
 
 if __name__ == "__main__":
     main()
