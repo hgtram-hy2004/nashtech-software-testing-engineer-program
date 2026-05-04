d67f179c8d8a5390e3746253584c547624cb4449
Nguyen Hang <ngthuyhang12122003@gmail.com>
2026-05-04 14:33:27 +0700

    Add function to calculate the side of a square

--- git/final/main.py
+++ git/final/main.py
@@ -1,6 +1,9 @@
 from Shapes import Shapes
 
 def main():
-
+    print("Side from area:", Shapes.calculate_square_side(area=64))        # 8.0
+    print("Side from perimeter:", Shapes.calculate_square_side(perimeter=20))  # 5.0
+    
 if __name__ == "__main__":
+    
     main()
