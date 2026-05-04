0813ef0b925608d0b074ec5e1393fab23bfe15b4
Nguyễn Xuân Đào <nxuandao1@gmail.com>
2026-05-04 14:50:50 +0700

    add method perimeterOfSquare into Shapes and update main.py

--- git/final/main.py
+++ git/final/main.py
@@ -3,7 +3,7 @@ from Shapes import Shapes
 def main():
     
     shapes = Shapes()
-    print("The perimeter of the square is:", shapes.perimeterOfSquare(7))
+    print("The perimeter of the square is:", shapes.perimeterOfSquare(8))
 
 
 if __name__ == "__main__":
