740ae0ae365dedb1e68fb38529dade2fdc729119
Nguyễn Xuân Đào <nxuandao1@gmail.com>
2026-05-04 14:37:33 +0700

    add method perimeterOfSquare into Shapes.py and update main.py

--- git/final/Shapes.py
+++ git/final/Shapes.py
@@ -2,6 +2,24 @@ import math
 
 
 class Shapes(object):
-    """
-        This class is used as utils to calculate properties of all shapes
-    """
+
+    # calculate the area of a rectangle
+
+    #   calculate the perimeter of a rectangle
+
+    #   calculate the width of a rectangle
+
+    #   calculate the length of a rectangle
+
+    #   calculate the area of square
+
+    #   calculate the perimeter of a square
+    def perimeterOfSquare(self, side):
+        return 4 * side
+
+    #   calculate the side of a square
+
+    #   calculate the area of a circle
+
+    #   calculate the radius of a circle
+
--- git/final/main.py
+++ git/final/main.py
@@ -1,6 +1,10 @@
 from Shapes import Shapes
 
 def main():
+    
+    shapes = Shapes()
+    print("The perimeter of the square is:", shapes.perimeterOfSquare(7))
+
 
 if __name__ == "__main__":
     main()
